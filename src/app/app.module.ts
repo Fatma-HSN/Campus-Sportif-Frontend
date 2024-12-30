94b7c2003045d5208da4acfb43442499153c5e3d
Fatma-HSN <fatma682amtaf@gmail.com>
2024-12-30 19:53:25 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,38 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AdherantComponent } from './adherant/adherant.component';
+import { ReservationComponent } from './reservation/reservation.component';
+import { TerrainComponent } from './terrain/terrain.component';
+import { AddAdherantComponent } from './add-adherant/add-adherant.component';
+import { AddResComponent } from './add-res/add-res.component';
+import { LayoutComponent } from './layout/layout.component';
+import { LoginComponent } from './login/login.component';
+import { provideHttpClient } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { AuthGuard } from './auth.guard';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdherantComponent,
+    ReservationComponent,
+    TerrainComponent,
+    AddAdherantComponent,
+    AddResComponent,
+    LayoutComponent,
+    LoginComponent
   ],
   imports: [
+    FormsModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    
   ],
   providers: [
-    provideClientHydration()
+    provideClientHydration(), provideHttpClient() ,AuthGuard
   ],
   bootstrap: [AppComponent]
 })
